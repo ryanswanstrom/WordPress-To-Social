415ea0e545053f6ccc9c03d6db8b3b27867c60d7
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2023-12-27 13:43:51 +0000

    change time back to 8:30

--- LinkedIn.cs
+++ LinkedIn.cs
@@ -11,7 +11,7 @@ namespace RyanSwanstrom.Function
         // An Azure Function to deploy WordPress Blog Post Content to LinkedIn
         // time hour 14 in UTC is 8am CST
         [FunctionName("LinkedIn")]
-        public void Run([TimerTrigger("17 */2 * * * *")]TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("17 30 14 * * *")]TimerInfo myTimer, ILogger log)
         {            
             log.LogInformation($"LinkedIn: checking for new blog posts: {DateTime.Now}");
             log.LogInformation($"This Azure Function will check the blog and post new stuff to LinkedIn");
