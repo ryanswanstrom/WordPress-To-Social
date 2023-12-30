260984e3c9f5809913854b9f38a90309b0dc1a47
Ryan Swanstrom <ryanswanstrom@users.noreply.github.com>
2023-12-30 02:03:50 +0000

    fcommenting out the Threads Azure Function for now

--- Threads.cs
+++ Threads.cs
@@ -8,19 +8,23 @@ namespace RyanSwanstrom.Function
 {
     public class Threads
     {
+        /*
         // An Azure Function to deploy WordPress Blog Post Content to Threads, awaiting API
         // time hour 14 in UTC is 8am CST
         [FunctionName("Threads")]
         public void Run([TimerTrigger("%TIMER_SCHEDULE%")]TimerInfo myTimer, ILogger log)
         {            
             log.LogInformation($"Threads: checking for new blog posts: {DateTime.Now}");
+            
             List<SocialPost> posts = BlogHelper.FindPosts(BlogHelper.THREADS_CAT, 1.0, log);
 
             foreach (SocialPost post in posts)
             {
                 //Post(post, log);
-            }                        
+            }  
+                                  
             log.LogInformation($"Threads: finished writing {posts.Count} blog posts to social media");
         }
+        */
     }
 }
